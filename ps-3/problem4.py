6d7095b506817983851a8217ed6667caf428d2d1
Sally8192 <104232365+Sally8192@users.noreply.github.com>
2023-09-26 10:15:41 -0400

    Update problem4.py

--- ps-3/problem4.py
+++ ps-3/problem4.py
@@ -1,38 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from random import random
 
 
-NTl = 1000            
-NPb = 0              
-tau = 3.053*60       
-h = 1.0               
-p = 1 - 2**(-h/tau)   
-tmax = 1000           
+total_atoms = 1000
+half_life_208Tl = 3.053 * 60  
 
-tpoints = np.arange(0.0,tmax,h)
-Tlpoints = []
-Pbpoints = []
+random_numbers = np.random.rand(total_atoms)
+
+decay_times = -half_life_208Tl * np.log(1 - random_numbers)
+
+sorted_decay_times = np.sort(decay_times)
 
 
-for t in tpoints:
-    Tlpoints.append(NTl)
-    Pbpoints.append(NPb)
-
-    
-    decay = 0
-    for i in range(NTl):
-        if random()<p:
-            decay += 1
-    NTl -= decay
-    NPb += decay
+max_time = np.max(sorted_decay_times)
+time_array = np.linspace(0, max_time, num=1000)
 
 
-plt.plot(tpoints,Tlpoints,label='Tl')
-plt.plot(tpoints,Pbpoints,label='Pb')
-plt.xlabel("Time(s)")
-plt.ylabel("Number of atoms")
+undecayed_atoms = np.zeros_like(time_array)
+
+
+for i, t in enumerate(time_array):
+    undecayed_atoms[i] = np.sum(sorted_decay_times > t)
+
+
+plt.figure(figsize=(10, 6))
+plt.plot(time_array, undecayed_atoms, label='Undecayed Atoms')
+plt.xlabel('Time (s)')
+plt.ylabel('Number of Undecayed Atoms')
 plt.title('Decay of 208Tl Over Time')
+
 plt.legend()
-plt.savefig('Decay2.png')
+plt.savefig("Decay2.png")
 plt.show()
